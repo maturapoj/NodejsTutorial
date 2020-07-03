0f4ed8bdf14edaf515148dcfe6e38417cbe2041a
maturapoj <maturapoj>
2020-07-03 17:51:39 +0700

    request form

--- NodejsBasic/routes/blog.js
+++ NodejsBasic/routes/blog.js
@@ -11,6 +11,12 @@ var router = express.Router();
     res.render('addblog');
   });
 
+  router.post('/add', function(req, res, next) {
+    console.log(req.body.name);
+    console.log(req.body.deccription);
+    console.log(req.body.author);
+  });
+
   router.get('/read', function(req, res, next) {
     res.render('readblog');
   });
