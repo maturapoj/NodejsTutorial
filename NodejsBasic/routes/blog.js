6dead5f2d985380ac5d8db394ec6a0f598a50990
maturapoj <maturapoj>
2020-07-03 19:26:04 +0700

    expresee-validator

--- NodejsBasic/routes/blog.js
+++ NodejsBasic/routes/blog.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const { render } = require('ejs');
 var router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 /* GET users listing. */
   router.get('/', function(req, res, next) {
@@ -11,10 +12,15 @@ var router = express.Router();
     res.render('addblog');
   });
 
-  router.post('/add', function(req, res, next) {
-    console.log(req.body.name);
-    console.log(req.body.deccription);
-    console.log(req.body.author);
+  router.post('/add',[
+    body("name","Please Input your blog name").not().isEmpty(),
+    body("description","Please Input your blog drsccription").not().isEmpty(),
+    body("author","Please Input your blog author").not().isEmpty(),
+  ], function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+    }
   });
 
   router.get('/read', function(req, res, next) {
