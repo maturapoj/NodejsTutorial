509e34b9a13e3a0cb3ff39858f42797c1655d3ea
maturapoj <maturapoj>
2020-07-07 01:27:54 +0700

    response Errors Object

--- NodejsBasic/routes/blog.js
+++ NodejsBasic/routes/blog.js
@@ -19,7 +19,7 @@ const { body, validationResult } = require('express-validator');
   ], function(req, res, next) {
     const result = validationResult(req);
     var errors = result.errors;
-    if (!errors.isEmpty()) {
+    if (!result.isEmpty()) {
       res.render('addblog',{errors:errors});
     }else{
       //insert to db
